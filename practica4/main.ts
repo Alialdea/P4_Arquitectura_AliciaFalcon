cf87c8e6aba9885073554385d2d3b5aeeea7b1ca
Alialdea <146476395+Alialdea@users.noreply.github.com>
2023-11-16 17:43:45 +0100

    Add files via upload

--- practica4/main.ts
+++ practica4/main.ts
@@ -22,11 +22,11 @@ import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
 const env = await load();
 
 const MONGO_URL = env.MONGO_URL || Deno.env.get("MONGO_URL");
-
+/*
 if (!MONGO_URL) {
   console.log("No mongo URL found");
   Deno.exit();
-}
+}*/
 
 await mongoose.connect(MONGO_URL);
 
